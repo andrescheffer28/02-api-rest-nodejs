eca60b59313e89bacca7b5540a2d089d095b1ab1
andreScheffer <andre.scheffer@ctjunior.com.br>
2025-06-22 12:09:01 -0300

    fix: corrigir criação do arquivo test.db

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,12 +1,11 @@
 import { config } from "dotenv"
 import { z } from "zod";
 
-if(process.env.NODE_ENV === 'test'){
-  config({ path: '.env.test' })
-}else{
+if (process.env.NODE_ENV === 'test') {
+  config({ path: '.env.test', override: true })
+} else {
   config()
 }
-
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
   DATABASE_URL: z.string(),
